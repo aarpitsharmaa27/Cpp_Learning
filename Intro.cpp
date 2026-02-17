8cdc5d31d7fff9e39fed7c91afe262c27aabeaa7
aarpitsharmaa27 <aarpitsharmaa27@gmail.com>
2026-02-18 01:58:40 +0530

    Initial C++ Portfolio Commit

---
+++ Intro.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  //Iostream is a header file that contain functions                    
+                    // for basic input and output
+    int main(){
+        std::cout << "Hiie Bro" << std::endl;
+        std::cout << "Its reallly Good" << std::endl;
+        return 0;
+    }
+
+
+
