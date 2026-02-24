9793738886bcbb3d1249b1d286f9daec2221c6c3
aarpitsharmaa27 <aarpitsharmaa27@gmail.com>
2026-02-24 13:45:53 +0530

    Using of def and using keyboard in C++

---
+++ typedef_using.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+typedef std::string text_t;     // we create a variable as text_t in place of std::string
+typedef int int_i;
+using flo_t = float;
+int main(){
+    text_t FirstName = "Arpit";     // now in place of std::string we can simply use text_t
+    std::cout << "My name is " << FirstName << "\n";
+    
+    int_i x = 45;
+    std::cout << "My weight is " << x  << "kg"<< "\n";
+    
+
+    flo_t v = 425.43;
+    std::cout << "My fat is " << v << "lbs " << "\n";
+     return 0 ;
+}
