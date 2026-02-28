6b1aa7caf407afd5810cc6d19d7daa4a2b577766
aarpitsharmaa27 <aarpitsharmaa27@gmail.com>
2026-02-28 12:06:15 +0530

    Maths functions

---
+++ Math_Functions.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <cmath>        // For math functions
+int main (){
+      
+    double x = 2;
+    double y = 3;
+    double z ;
+    
+
+    // z = std :: max(x, y)  ; Maximum value
+    // z = std :: min(x, y) ;   Minimum Value
+    // z = pow(x,y) ;           Power or x raise to y
+    // z = pow (4,7);           Power or 4 raise to 7
+    // z = sqrt (25);           It gives 25 is square of what
+    // z = abs(324.34);         It gives absolute value i.e -324 = 324, 234.3 = 234
+    z = round(234.56);       It round value to nearest number
+    //z = ceil (9.01);          It round value to next number
+
+    std :: cout << z;
+   
+    
+    return 0;
+}
